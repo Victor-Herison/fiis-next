fe9008e16f716d346fd514c986d6c362c0329fda
Victor-Herison <victorherisondev@gmail.com>
2025-03-18 11:36:04 -0300

    metadata

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,18 @@
+export const metadata = {
+  title: "Filtros de Fii",
+  description: "Site que filtra fundos imobiliarios.",
+  keywords: "Fundos imobiliarios, filtro, investimento, dicas, FIIs, FII, fiis",
+  icons: {
+    icon: "/icon.svg", // Caminho para o favicon
+  },
+};
+
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <body>{children}</body>
     </html>
   )
