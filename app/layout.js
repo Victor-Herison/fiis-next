c0677aef21b3902f40e93eb820783da81efbd343
Victor-Herison <victorherisondev@gmail.com>
2025-03-20 17:45:27 -0300

    Header

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Header from "@/components/header";
+
 export const metadata = {
   title: "Filtros de Fii",
   description: "Site que filtra fundos imobiliarios.",
@@ -12,8 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+        
+        </body>
     </html>
   )
 }
