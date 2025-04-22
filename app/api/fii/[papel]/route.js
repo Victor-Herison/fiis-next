4ab6be09295ed3149438fca4631a25656e6fe79b
victor-herison <victorherisondev@gmail.com>
2025-04-21 22:50:21 -0300

    fixing

--- app/api/fii/[papel]/route.js
+++ app/api/fii/[papel]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import FiiModel from "@/utils/FiiModel";
+import {FiiModel} from "@/utils/schema";
 import connectDB from "@/utils/db";
 
 export async function GET(req, { params }) {
