97e16a9ca0969372e7639a8f8c12efd1c1034a87
Victor-Herison <victorherisondev@gmail.com>
2025-04-30 22:43:35 -0300

    google console search

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        
+      <meta name="google-site-verification" content="gOdsY24DgZ0uxVIJS-chvNIVGeWDe_iajLtBz7ZONKI" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7395370785593703"
         ></script></head>
       <body className="bg-gray-900 p-0 m-0">
