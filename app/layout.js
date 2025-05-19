914b0deb4242b26f2c48576cd7924e0c5fdf455c
Victor-Herison <victorherisondev@gmail.com>
2025-05-19 14:21:23 -0300

    Poisicionamento do ADS

--- app/layout.js
+++ app/layout.js
@@ -26,8 +26,7 @@ export default function RootLayout({ children }) {
         </head>
       <body className="bg-gray-900 p-0 m-0" suppressHydrationWarning>
         <Header />
-        <main>      
-          <ins className="adsbygoogle block"
+        <ins className="adsbygoogle block"
               
               data-ad-client="ca-pub-7395370785593703"
               data-ad-slot="6442136295"
@@ -36,6 +35,8 @@ export default function RootLayout({ children }) {
           <script>
               (adsbygoogle = window.adsbygoogle || []).push({});
           </script>
+        <main>      
+          
           {children}
           <SpeedInsights />
         </main>
